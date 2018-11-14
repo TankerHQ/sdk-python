e1d20d037a7a5329e3ddf11fe852ea2d23773f67
jerem <jeremy.tellaa@tanker.io>
2018-11-14 12:14:01 +0100

    add CI on mac

--- run-ci.py
+++ run-ci.py
@@ -9,32 +9,32 @@ import ci.dmenv
 import ci.git
 
 
-def run_setup_py(src_path, *args):
+def run_setup_py(src_path, profile, *args):
     env = os.environ.copy()
-    env["TANKER_NATIVE_BUILD_PATH"] = "../Native/build/gcc8/x86_64/Release"
+    env["TANKER_NATIVE_BUILD_PATH"] = f"../Native/build/{profile}/x86_64/Release"
     ci.dmenv.run("python", "setup.py", *args, env=env, cwd=src_path)
 
 
-def build(*, workspace, src):
+def build(*, workspace, src, profile):
     ci.git.prepare_sources(
         workspace=workspace, repos=["python", "Native"], submodule=False, clean=True
     )
     with workspace / "Native":
-        builder = ci.cpp.Builder(profile="gcc8", bindings=True, coverage=False)
+        builder = ci.cpp.Builder(profile=profile, bindings=True, coverage=False)
         builder.install_deps()
         builder.build()
 
     python_src_path = workspace / "python"
     ci.dmenv.install(cwd=python_src_path, develop=False)
-    run_setup_py(python_src_path, "develop")
+    run_setup_py(python_src_path, profile, "develop")
 
 
 def test(*, cwd):
     ci.dmenv.run("pytest", "-s", cwd=cwd)
 
 
-def deploy(*, cwd):
-    run_setup_py(cwd, "bdist_wheel")
+def deploy(*, cwd, profile):
+    run_setup_py(cwd, profile, "bdist_wheel")
     build_dir = cwd / "dist"
     wheels = build_dir.files("tankersdk-*.whl")
     if len(wheels) != 1:
@@ -43,22 +43,27 @@ def deploy(*, cwd):
     ci.run("scp", wheel, "pypi@10.100.0.1:packages")
 
 
-def runner(steps):
+def runner(steps, profile):
     workspace = Path("~/work").expanduser()
     src = Path.abspath(Path(__file__)).parent
     python_repo = workspace / "python"
-    build(workspace=workspace, src=src)
+    build(workspace=workspace, src=src, profile=profile)
     if "test" in steps:
         test(cwd=python_repo)
     if "deploy" in steps:
-        deploy(cwd=python_repo)
+        deploy(cwd=python_repo, profile=profile)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("steps", nargs="+", choices=["test", "deploy"])
+    parser.add_argument("--runner", required=True)
     args = parser.parse_args()
-    runner(args.steps)
+    if args.runner == "linux":
+        profile = "gcc8"
+    elif args.runner == "macos":
+        profile = "macos"
+    runner(args.steps, profile)
 
 
 if __name__ == "__main__":
