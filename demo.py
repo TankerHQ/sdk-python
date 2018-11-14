76f7e1d3703dec5a28a80c06a4c72c3ab3fda8f0
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-11-14 15:58:38 +0100

    demo: remove validation code
    No longer works :P

--- demo.py
+++ demo.py
@@ -9,18 +9,14 @@ TRUSTCHAIN_ID = "Lj93QRsF4aadPoT/abm/ZUS1YqC5StX+B326stEeiT8="
 TRUSTCHAIN_PRIVATE_KEY = "TZEIID4mTQta0xtLSNzh/mejX1SOjLb4l8tLCbORHpk29v9yEMS7uk0s9GlbPtuqn5+morIna1Op2F8Y6uTl2Q=="  # noqa
 
 
-def on_waiting_for_validation(code):
-    print("Please add device with the following code", code)
 
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("user_id")
     parser.add_argument("--storage-path", default="")
-    parser.add_argument("--validation-code")
     args = parser.parse_args()
     user_id = args.user_id
-    validation_code = args.validation_code
     storage_path = args.storage_path
 
     tanker = Tanker(
@@ -29,12 +25,9 @@ async def main():
         trustchain_private_key=TRUSTCHAIN_PRIVATE_KEY,
         writable_path=storage_path,
     )
-    tanker.on_waiting_for_validation = on_waiting_for_validation
     token = tanker.generate_user_token(user_id)
 
     await tanker.open(user_id, token)
-    if validation_code:
-        await tanker.accept_device(validation_code.encode())
 
     message = b"I love you"
     encrypted = await tanker.encrypt(message)
