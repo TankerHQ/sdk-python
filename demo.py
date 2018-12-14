0abe9640ab5e16763fe8471eeea487c71c16b9a2
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-12-14 16:38:10 +0100

    demo: Add 'share' and 'signup' commands
    Also, use request.Session so that session cookies work

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 import argparse
 import asyncio
 import json
@@ -16,25 +16,41 @@ def load_config(cfg_path: Path) -> Dict[str, str]:
     return json.loads(cfg_path.text())  # type: ignore
 
 
-def do_request(method: str, segment: str, **kwargs: Any) -> requests.Response:
-    return requests.request(method, f"{SERVER_URL}/{segment}", **kwargs)
+def do_request(
+    session: requests.Session, method: str, segment: str, **kwargs: Any
+) -> requests.Response:
+    full_url = f"{SERVER_URL}/{segment}"
+    func = getattr(session, method)
+    return func(full_url, **kwargs)  # type: ignore
 
 
-async def open_session(email: str, password: str, signup: bool = False) -> Tanker:
+async def open_tanker_session(
+    requests_session: requests.Session, email: str, password: str, signup: bool = False
+) -> Tanker:
     storage_path = Path("~/.local/share/tanker").expanduser() / email
     storage_path.makedirs_p()
 
-    config = do_request("get", "config").json()
+    config = do_request(requests_session, "get", "config").json()
     tanker = Tanker(
         config["trustchainId"], trustchain_url=config["url"], writable_path=storage_path
     )
 
     if signup:
-        res = do_request("post", "signup", json={"email": email, "password": password})
+        res = do_request(
+            requests_session,
+            "post",
+            "signup",
+            json={"email": email, "password": password},
+        )
         if not res.ok:
             sys.exit(f"Could not signup: {res.text}")
     else:
-        res = do_request("post", "login", json={"email": email, "password": password})
+        res = do_request(
+            requests_session,
+            "post",
+            "login",
+            json={"email": email, "password": password},
+        )
         if not res.ok:
             sys.exit(f"Could not login: {res.text}")
 
@@ -44,15 +60,25 @@ async def open_session(email: str, password: str, signup: bool = False) -> Tanke
     return tanker
 
 
+def get_user_ids(requests_session: requests.Session, emails: List[str]) -> List[str]:
+    all_users = do_request(requests_session, "get", "users").json()
+    by_email = dict([(x["email"], x["id"]) for x in all_users])
+    return [by_email[x] for x in emails]
+
+
 async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("email")
-    parser.add_argument("--signup", action="store_true")
     parser.add_argument("--password", required=True)
 
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
+
+    subparsers.add_parser("signup")
+
     encrypt_parser = subparsers.add_parser("encrypt")
+    encrypt_parser.add_argument("-m", "--message", required=True)
     encrypt_parser.add_argument("-o", "--output", required=True)
+    encrypt_parser.add_argument("--user", action="append", dest="users")
 
     decrypt_parser = subparsers.add_parser("decrypt")
     decrypt_parser.add_argument("input")
@@ -60,21 +86,34 @@ async def main() -> None:
     args = parser.parse_args()
     email = args.email
     password = args.password
-    signup = args.signup
     command = args.command
 
     if not command:
         parser.print_help()
         sys.exit(1)
 
-    print("Opening session ...")
-    tanker = await open_session(email, password=password, signup=signup)
+    requests_session = requests.Session()
+
+    if args.command == "signup":
+        print("Creating new user")
+        await open_tanker_session(
+            requests_session, email, password=password, signup=True
+        )
+        print("OK")
+        return
+
+    print("Opening new session")
+    tanker = await open_tanker_session(
+        requests_session, email, password=password, signup=False
+    )
     print("Done!")
 
     if args.command == "encrypt":
-        message = input("Please enter your message below:\n")
+        user_emails = args.users
+        user_ids = get_user_ids(requests_session, user_emails)
+        message = args.message
         input_bytes = message.encode()
-        encrypted = await tanker.encrypt(input_bytes)
+        encrypted = await tanker.encrypt(input_bytes, share_with_users=user_ids)
         output_path = Path(args.output)
         output_path.write_bytes(encrypted)
         print("Encrypted data written to", output_path)
