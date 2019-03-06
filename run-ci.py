c78a59a112910de2fb4efbee037144763be8796f
jerem <jeremy.tellaa@tanker.io>
2019-03-06 18:17:08 +0100

    use ci module

--- run-ci.py
+++ run-ci.py
@@ -1,57 +1,10 @@
 import argparse
-import os
 import sys
 
-from path import Path
 
-import ci
 import ci.cpp
-import ci.dmenv
 import ci.git
-import ci.tanker_configs
-
-
-def run_setup_py(src_path: Path, profile: str, *args: str) -> None:
-    env = os.environ.copy()
-    env["TANKER_NATIVE_BUILD_PATH"] = f"../sdk-native/build/{profile}"
-    ci.dmenv.run("python", "setup.py", *args, env=env, cwd=src_path)
-
-
-def build(workspace: Path, *, profile: str) -> None:
-    ci.cpp.update_conan_config()
-    with workspace / "sdk-native":
-        ci.cpp.check(profile=profile, coverage=False, run_tests=False)
-
-    python_src_path = workspace / "sdk-python"
-    run_setup_py(python_src_path, profile, "clean", "develop")
-
-
-def check(python_src_path: Path) -> None:
-    with python_src_path:
-        env = os.environ.copy()
-        env["TANKER_CONFIG_NAME"] = "dev"
-        env["TANKER_CONFIG_FILEPATH"] = ci.tanker_configs.get_path()
-        env["MYPYPATH"] = python_src_path / "stubs"
-        ci.dmenv.run("python", "lint.py", env=env)
-        ci.dmenv.run("pytest", "--verbose", "--capture=no", env=env)
-
-
-def deploy(python_src_path: Path, *, profile: str, git_tag: str) -> None:
-    with python_src_path:
-        version = ci.bump.version_from_git_tag(git_tag)
-        ci.bump.bump_files(version)
-    dist_path = python_src_path / "dist"
-    dist_path.rmtree_p()
-    run_setup_py(python_src_path, profile, "bdist_wheel")
-    wheels = dist_path.files("tankersdk-*.whl")
-    if len(wheels) != 1:
-        raise Exception("multiple wheels found: {}".format(wheels))
-    wheel_path = wheels[0]
-    if sys.platform == "win32":
-        wheel_path = wheel_path.lower()
-        wheel_path = wheel_path.replace(os.path.sep, "/")
-        wheel_path = wheel_path.replace("c:/", "/c/")
-    ci.run("scp", wheel_path, "pypi@tanker.local:packages")
+import ci.sdk_python
 
 
 def main() -> None:
@@ -68,7 +21,6 @@ def main() -> None:
     check_parser.add_argument("--profile", required=True)
 
     deploy_parser = subparsers.add_parser("deploy")
-    deploy_parser.add_argument("--git-tag", required=True)
     deploy_parser.add_argument("--profile", required=True)
 
     subparsers.add_parser("mirror")
@@ -87,19 +39,16 @@ def main() -> None:
         ci.git.mirror(github_url="git@github.com:TankerHQ/sdk-python")
         return
 
-    profile = args.profile
-    workspace = ci.git.prepare_sources(repos=["sdk-native", "sdk-python"])
-    python_src_path = workspace / "sdk-python"
-    ci.dmenv.install(cwd=python_src_path, develop=False)
+    builder = ci.sdk_python.Builder(profile=args.profile)
 
-    if profile != "windows":
-        build(workspace, profile=profile)
+    if args.profile != "windows":
+        builder.build()
 
     if command == "check":
-        check(python_src_path)
+        builder.lint()
+        builder.check()
     elif command == "deploy":
-        git_tag = args.git_tag
-        deploy(python_src_path, profile=profile, git_tag=git_tag)
+        builder.deploy()
 
 
 if __name__ == "__main__":
