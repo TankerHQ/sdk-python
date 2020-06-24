6d9122ef19969ed3d4a63a291b3ecf2f48158d9b
Philippe Daouadi <philippe.daouadi@tanker.io>
2020-06-24 14:19:18 +0200

    cffi: remove all documentation
    It is duplicated and unmaintained, read the official header for
    documentation.

--- cffi_defs.h
+++ cffi_defs.h
@@ -6,73 +6,27 @@ typedef struct tanker_promise tanker_promise_t;
 typedef struct tanker_error tanker_error_t;
 
 typedef void* (*tanker_future_then_t)(tanker_future_t* fut, void* arg);
-/*!
- * Create a new empty promise.
- * \remark must call tanker_promise_destroy() to get rid of it.
- */
+
 tanker_promise_t* tanker_promise_create(void);
 
-/*!
- * Destroy a promise.
- * \pre promise must be allocated with tanker_promise_create().
- */
 void tanker_promise_destroy(tanker_promise_t* promise);
 
-/*!
- * Get a future from a promise.
- * \pre promise parameter must be allocated with tanker_promise_create().
- * \remark must call tanker_future_destroy"()" to get rid of the returned
- *         future.
- */
 tanker_future_t* tanker_promise_get_future(tanker_promise_t* promise);
 
-/*!
- * Set a promise value.
- * \pre promise parameter must be allocated with tanker_promise_create().
- */
 void tanker_promise_set_value(tanker_promise_t* promise, void* value);
 
-/*!
- * Get the content of the future.
- * \return The void pointer representing the value. Refer to the documentation
- * of the function returning the future to know how to interpret the value.
- */
 void* tanker_future_get_voidptr(tanker_future_t* future);
 
-/*!
- * Returns 1 if the future is ready, 0 otherwise.
- */
 bool tanker_future_is_ready(tanker_future_t* future);
 
-/*!
- * Block until the future is ready.
- * \pre future parameter must be allocated with tanker API.
- */
 void tanker_future_wait(tanker_future_t* future);
 
-/*!
- * Set a callback to the future chain.
- * \remark For the moment adding multiple callbacks is undefined
- * \param arg arguments for the callback.
- * \return A new future with the callback.
- * \remark The future returned has to be freed with tanker_future_destroy().
- */
 tanker_future_t* tanker_future_then(tanker_future_t* future,
                                     tanker_future_then_t cb,
                                     void* arg);
 
-/*!
- * Get the future error if any.
- *
- * \return The error contained in the future or NULL if there was no error.
- */
 tanker_error_t* tanker_future_get_error(tanker_future_t* future);
 
-/*!
- * Check if there is an error in the future.
- *
- * \return 0 if the future has no error, any other value otherwise.
- */
 unsigned char tanker_future_has_error(tanker_future_t* future);
 
 void tanker_future_destroy(tanker_future_t* future);
@@ -158,36 +112,24 @@ typedef struct tanker_verification_method_list
     tanker_verification_method_list_t;
 typedef struct tanker_attach_result tanker_attach_result_t;
 
-/*!
- * \brief The list of a user's devices
- */
 struct tanker_device_list
 {
   tanker_device_list_elem_t* devices;
   uint32_t count;
 };
 
-/*!
- * \brief Describes one device belonging to the user
- */
 struct tanker_device_list_elem
 {
   char const* device_id;
   bool is_revoked;
 };
 
-/*!
- * \brief The list of a user verification methods
- */
 struct tanker_verification_method_list
 {
   tanker_verification_method_t* methods;
   uint32_t count;
 };
 
-/*!
- * \brief a struct describing a log message
- */
 struct tanker_log_record
 {
   char const* category;
@@ -197,20 +139,11 @@ struct tanker_log_record
   char const* message;
 };
 
-/*!
- * \brief Callback type to filter Tanker SDK logs.
- * \discussion Should be used with tanker_set_log_handler.
- *
- * \param record a struct containing all message informations
- */
 typedef void (*tanker_log_handler_t)(tanker_log_record_t const* record);
 
 typedef struct tanker_connection tanker_connection_t;
 typedef void (*tanker_event_callback_t)(void* arg, void* data);
 
-/*!
- * Options used to create a tanker instance.
- */
 struct tanker_options
 {
   uint8_t version;
@@ -258,11 +191,6 @@ struct tanker_encrypt_options
   uint32_t nb_recipient_gids;
 };
 
-/*!
- * \brief a struct containing the result of an attach_provisional_identity()
- * If the status is TANKER_STATUS_READY, the method will be default initialized
- * with the values in TANKER_VERIFICATION_METHOD_INIT
- */
 struct tanker_attach_result
 {
   uint8_t version;
@@ -272,265 +200,66 @@ struct tanker_attach_result
   tanker_verification_method_t* method;
 };
 
-/*!
- * Allow to access version.
- * \return The current version of Tanker as a string.
- */
 char const* tanker_version_string(void);
 
-/*!
- * Set the log handler of the API with a function pointer
- * \param handler the function pointer, it must have the prototype of
- *        tanker_log_handler_t.
- */
 void tanker_set_log_handler(tanker_log_handler_t handler);
 
-/*!
- * Initialize the SDK
- */
 void tanker_init(void);
 
-/*!
- * Create a Tanker instance.
- * \param options struct tanker_options_t with the following preconditions.
- * \pre The *option* structure must not be NULL, as well as the fields
- *      specified in its documentation.
- * \return A tanker_future of a tanker_t*
- * \throws TANKER_ERROR_INVALID_ARGUMENT \p options is NULL, or lacks mandatory
- *         fields, or has malformed fields
- */
 tanker_future_t* tanker_create(tanker_options_t const* options);
 
-/*!
- * Destroy a tanker instance.
- * \param tanker a tanker tanker_t* to be deleted.
- * \pre The tanker parameter has been allocated.
- * \return an async future.
- */
 tanker_future_t* tanker_destroy(tanker_t* tanker);
 
-/*!
- * Connect to an event.
- * \param tanker A tanker tanker_t* instance.
- * \param event The event to connect.
- * \param data The data to pass to the callback.
- * \return an expected of a tanker_connection_t* that must be disconnected with
- * tanker_event_disconnect().
- * \throws TANKER_ERROR_INVALID_ARGUMENT \p event does not exist
- */
 tanker_expected_t* tanker_event_connect(tanker_t* tanker,
                                         enum tanker_event event,
                                         tanker_event_callback_t cb,
                                         void* data);
 
-/*!
- * Disconnect from an event.
- * \param tanker is not yet used.
- * \param event The event to disconnect.
- * \return an expected of NULL.
- */
 tanker_expected_t* tanker_event_disconnect(tanker_t* tanker,
                                            enum tanker_event event);
 
-/*!
- * Sign up to Tanker.
- *
- * \param tanker a tanker tanker_t* instance.
- * \param identity the user identity.
- * \return a future of NULL
- * \throws TANKER_ERROR_INVALID_ARGUMENT \p indentity is NULL
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server
- * or the server returned an error
- * \throws TANKER_ERROR_OTHER could not open the local storage
- */
 tanker_future_t* tanker_start(tanker_t* tanker, char const* identity);
 
-/*!
- * Register a verification method associated with an identity.
- *
- * \param tanker a tanker tanker_t* instance.
- * \param verification the verification methods to set up for the
- * user, or NULL.
- * \return a future of NULL
- * \throws TANKER_ERROR_INVALID_VERIFICATION_KEY unlock key is incorrect
- * \throws TANKER_ERROR_INVALID_VERIFICATION_CODE verification code is incorrect
- * \throws TANKER_ERROR_INVALID_UNLOCK_PASSWORD passphrase is incorrect
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server
- * or the server returned an error
- * \throws TANKER_ERROR_OTHER could not open the local storage
- */
 tanker_future_t* tanker_register_identity(
     tanker_t* tanker, tanker_verification_t const* verification);
 
-/*!
- * Verify an identity with provided verification.
- *
- * \param tanker a tanker tanker_t* instance.
- * \param verification the verification methods to set up for the
- * user, or NULL.
- * \return a future of NULL
- * \throws TANKER_ERROR_INVALID_VERIFICATION_KEY unlock key is incorrect
- * \throws TANKER_ERROR_INVALID_VERIFICATION_CODE verification code is incorrect
- * \throws TANKER_ERROR_INVALID_UNLOCK_PASSWORD passphrase is incorrect
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server
- * or the server returned an error
- * \throws TANKER_ERROR_OTHER could not open the local storage
- */
 tanker_future_t* tanker_verify_identity(
     tanker_t* tanker, tanker_verification_t const* verification);
 
-/*!
- * Close a tanker session.
- * \param tanker A tanker tanker_t* instance.
- * \pre tanker must be allocated with tanker_create().
- * \pre tanker must be opened with tanker_open().
- */
 tanker_future_t* tanker_stop(tanker_t* tanker);
 
-/*!
- * The current Tanker status.
- * \param tanker A tanker tanker_t* instance.
- * \pre tanker must be allocated with tanker_create().
- * \return the current Tanker status.
- */
 enum tanker_status tanker_status(tanker_t* tanker);
 
-/*!
- * Get the current device id.
- * \param session A tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \return a future of char* that must be freed with tanker_free_buffer.
- */
 tanker_future_t* tanker_device_id(tanker_t* session);
 
-/*!
- * Get the list of the user's devices.
- * \param session A tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \return a future of tanker_device_list_t* that must be freed with
- * tanker_free_device_list.
- */
 tanker_future_t* tanker_get_device_list(tanker_t* session);
 
-/*!
- * Generate an verificationKey that can be used to accept a device
- * \param session A tanker tanker_t* instance
- * \pre tanker_status == TANKER_STATUS_READY
- * \return a future of char* that must be freed with tanker_free_buffer
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server or the
- * server returned an error
- */
 tanker_future_t* tanker_generate_verification_key(tanker_t* session);
 
-/*!
- * Registers, or updates, the user's unlock claims,
- * creates an unlock key if necessary
- * \param session a tanker tanker_t* instance
- * \param verification a instance of tanker_verification_t
- * \pre tanker_status == TANKER_STATUS_READY
- * \return a future to void
- */
 tanker_future_t* tanker_set_verification_method(
     tanker_t* session, tanker_verification_t const* verification);
 
-/*!
- * Return all registered verification methods for the current user.
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \return a tanker_verification_method_list_t*
- */
 tanker_future_t* tanker_get_verification_methods(tanker_t* session);
 
-/*!
- * Get the encrypted size from the clear size.
- * Must be called before encrypt to allocate the encrypted buffer.
- */
 uint64_t tanker_encrypted_size(uint64_t clear_size);
 
-/*!
- * Get the decrypted size.
- *
- * Must be called before decrypt to allocate the decrypted buffer.
- *
- * \return The size the decrypted data would take, cast to a void*, or an
- * error if the data is corrupted.
- * \throws TANKER_ERROR_DECRYPT_FAILED the
- * buffer is corrupt or truncated
- */
 tanker_expected_t* tanker_decrypted_size(uint8_t const* encrypted_data,
                                          uint64_t encrypted_size);
 
-/*!
- * Get the resource id from an encrypted data.
- * \return an already ready future of a string.
- */
 tanker_expected_t* tanker_get_resource_id(uint8_t const* encrypted_data,
                                           uint64_t encrypted_size);
 
-/*!
- * Encrypt data.
- * \param tanker A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param encrypted_data The container for the encrypted data.
- * \pre encrypted_data must be allocated with a call to
- *      tanker_encrypted_size() in order to get the size beforehand.
- * \param data The array of bytes to encrypt.
- * \pre data_size must be the size of the *data* parameter
- *
- * \return An empty future.
- * \throws TANKER_ERROR_USER_NOT_FOUND at least one user to share with was not
- * found
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server or the
- * server returned an error
- */
 tanker_future_t* tanker_encrypt(tanker_t* tanker,
                                 uint8_t* encrypted_data,
                                 uint8_t const* data,
                                 uint64_t data_size,
                                 tanker_encrypt_options_t const* options);
 
-/*!
- * Decrypt an encrypted data.
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param decrypted_data Decrypted array of bytes.
- * \pre decrypted_data must be allocated with a call to
- *      tanker_decrypted_size() in order to get the size beforehand.
- * \param data Array of bytes to decrypt.
- * \param data_size Size of the \p data argument.
- *
- * \return An empty future.
- * \throws TANKER_ERROR_DECRYPT_FAILED The buffer was corrupt or truncated
- * \throws TANKER_ERROR_RESOURCE_KEY_NOT_FOUND The key was not found
- */
 tanker_future_t* tanker_decrypt(tanker_t* session,
                                 uint8_t* decrypted_data,
                                 uint8_t const* data,
                                 uint64_t data_size);
 
-/*!
- * Share a symetric key of an encrypted data with other users.
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param recipient_public_identities Array containing the recipients' public
- * identities.
- * \param nb_recipient_public_identities The number of recipients in
- * recipient_public_identities.
- * \param recipient_gids Array of strings describing the recipient groups.
- * \param nb_recipient_gids The number of groups in recipient_gids.
- * \param resource_ids Array of string describing the resources.
- * \param nb_resource_ids The number of resources in resource_ids.
- *
- * \return An empty future.
- * \throws TANKER_ERROR_RECIPIENT_NOT_FOUND One of the recipients was not found,
- * no action was done
- * \throws TANKER_ERROR_RESOURCE_KEY_NOT_FOUND One of the
- * resource keys was not found, no action was done
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server or the
- * server returned an error
- */
 tanker_future_t* tanker_share(tanker_t* session,
                               char const* const* recipient_public_identities,
                               uint64_t nb_recipient_public_identities,
@@ -539,52 +268,12 @@ tanker_future_t* tanker_share(tanker_t* session,
                               char const* const* resource_ids,
                               uint64_t nb_resource_ids);
 
-/*!
- * Attach a provisional identity to the current user
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param provisional_identity provisional identity you want to claim.
- *
- * \return A future of tanker_attach_result_t*.
- * \throws TANKER_ERROR_NOTHING_TO_CLAIM there is nothing to claim for this
- * identity
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server or
- * the server returned an error
- */
 tanker_future_t* tanker_attach_provisional_identity(
     tanker_t* session, char const* provisional_identity);
 
-/*!
- * Verifies a provisional identity to the current user
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param verification the verification used to verify this provisional
- * identity.
- *
- * \return An empty future.
- * \throws TANKER_ERROR_NOTHING_TO_CLAIM there is nothing to claim for this
- * identity
- * \throws TANKER_ERROR_OTHER could not connect to the Tanker server or
- * the server returned an error
- */
 tanker_future_t* tanker_verify_provisional_identity(
     tanker_t* ctanker, tanker_verification_t const* verification);
 
-/*!
- * Revoke a device by device id.
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param device_id the device identifier as returned by tanker_device_id().
- *
- * \return An empty future.
- * \throws TANKER_DEVICE_NOT_FOUND The device_id in parameter does not
- * corresponds to a valid device
- * \throws TANKER_INVALID_ARGUMENT The device_id in parameter correspond to
- * another user's device.
- */
 tanker_future_t* tanker_revoke_device(tanker_t* session, char const* device_id);
 
 void tanker_free_buffer(void const* buffer);
@@ -594,70 +283,17 @@ void tanker_free_device_list(tanker_device_list_t* list);
 void tanker_free_verification_method_list(
     tanker_verification_method_list_t* list);
 
-/*!
- * Hash a password before sending it to the application server where it will
- * be hashed again.
- *
- * If you decide to synchronize the Tanker passphrase with the user password,
- * you will need to hash it client-side (in addition to hashing it server-side,
- * as explained in the "Good practices" section of the documentation). This
- * function allows you to do that hashing.
- *
- * \warning This is not a password hash function, it is only used to
- * solve the specific problem of passphrase synchronization described above, a
- * proper password hash function is still needed server-side. Please read the
- * documentation for more detail.
- *
- * \param password the password to prehash
- *
- * \return an expected of the prehashed password which must be freed with
- * tanker_free_buffer
- *
- * \throws TANKER_ERROR_INVALID_ARGUMENT \p password is null or empty
- */
 tanker_expected_t* tanker_prehash_password(char const* password);
 
 void tanker_free_attach_result(tanker_attach_result_t* result);
+
 // ctanker/groups.h
 
-/*!
- * Create a group containing the given users.
- * Share a symetric key of an encrypted data with other users.
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param public_identities_to_add Array of the group members' public
- * identities. \param nb_public_identities_to_add The number of members in
- * public_identities_to_add.
- *
- * \return A future of the group ID as a string.
- * \throws TANKER_ERROR_USER_NOT_FOUND One of the members was not found, no
- * action was done
- * \throws TANKER_ERROR_INVALID_GROUP_SIZE The group is either empty, or has too
- * many members
- */
 tanker_future_t* tanker_create_group(
     tanker_t* session,
     char const* const* public_identities_to_add,
     uint64_t nb_public_identities_to_add);
 
-/*!
- * Updates an existing group, referenced by its groupId,
- * adding the user identified by their user Ids to the group's members.
- *
- * \param session A tanker tanker_t* instance.
- * \pre tanker_status == TANKER_STATUS_READY
- * \param group_id The group ID returned by tanker_create_group
- * \param public_identities_to_add Array of the new group members' public
- * identities. \param nb_public_identities_to_add The number of users in
- * public_identities_to_add.
- *
- * \return An empty future.
- * \throws TANKER_ERROR_USER_NOT_FOUND One of the users was not found, no
- * action was done
- * \throws TANKER_ERROR_INVALID_GROUP_SIZE Too many users were added to the
- * group.
- */
 tanker_future_t* tanker_update_group_members(
     tanker_t* session,
     char const* group_id,
