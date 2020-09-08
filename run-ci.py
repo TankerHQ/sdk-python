52011c014d9328edf561b9769a01b916dbc8ad36
jerem <jeremy.tellaa@tanker.io>
2020-09-08 14:37:50 +0200

    wip

--- run-ci.py
+++ run-ci.py
@@ -74,7 +74,24 @@ class Builder:
         # so that they can be found even when the working directory
         # changes, and we make sure *all* paths used in build_tanker.py
         # are absolute
-        tankerci.run("poetry", "publish", "--build", "--repository", "gitlab", env=env)
+        tankerci.run("poetry", "build", env=env)
+        wheels = dist_path.files("tankersdk-*.whl")
+        if len(wheels) != 1:
+            raise Exception("multiple wheels found: {}".format(wheels))
+        wheel_path = wheels[0]
+        env["TWINE_PASSWORD"] = env["GITLAB_TOKEN"]
+        env["TWINE_USERNAME"] = env["GITLAB_USERNAME"]
+        repository = env["POETRY_REPOSITORIES_GITLAB_URL"]
+        tankerci.run(
+            "poetry",
+            "run",
+            "twine",
+            "upload",
+            "--repository-url",
+            repository,
+            wheel_path,
+            env=env,
+        )
 
 
 def build(tanker_source: TankerSource, profile: str) -> Builder:
