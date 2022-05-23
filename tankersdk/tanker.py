ce4b8b456c73448bfbc14e616ec1e41a4c97c0da
Timothée Isnard <timothee.isnard@tanker.io>
2022-05-23 15:29:23 +0200

    feat: Add E2ePassphrase support
    This upgrades the CVerification type to version 6 and the
    CVerificationOptions to version 2,
    which is required for compatibility with new sdk-native versions

--- tankersdk/tanker.py
+++ tankersdk/tanker.py
@@ -54,6 +54,7 @@ class VerificationMethodType(Enum):
     PHONE_NUMBER = 5
     PREVERIFIED_EMAIL = 6
     PREVERIFIED_PHONE_NUMBER = 7
+    E2E_PASSPHRASE = 8
 
 
 class Verification:
@@ -64,8 +65,13 @@ class Verification:
 
 
 class VerificationOptions:
-    def __init__(self, with_session_token: bool):
+    def __init__(
+        self,
+        with_session_token: bool = False,
+        allow_e2e_method_switch: bool = False,
+    ):
         self.with_session_token = with_session_token
+        self.allow_e2e_method_switch = allow_e2e_method_switch
 
 
 class EmailVerification(Verification):
@@ -96,6 +102,13 @@ class PassphraseVerification(Verification):
 
     def __init__(self, passphrase: str):
         self.passphrase = passphrase
+
+
+class E2ePassphraseVerification(Verification):
+    method_type = VerificationMethodType.E2E_PASSPHRASE
+
+    def __init__(self, e2e_passphrase: str):
+        self.e2e_passphrase = e2e_passphrase
 
 
 class VerificationKeyVerification(Verification):
@@ -302,10 +315,15 @@ class CVerificationOptions:
     def __init__(
         self,
         with_session_token: bool,
+        allow_e2e_method_switch: bool,
     ):
         self._c_data = ffi.new(
             "tanker_verification_options_t *",
-            {"version": 1, "with_session_token": with_session_token},
+            {
+                "version": 2,
+                "with_session_token": with_session_token,
+                "allow_e2e_method_switch": allow_e2e_method_switch,
+            },
         )
 
     def get(self) -> CData:
@@ -322,7 +340,7 @@ class CVerification:
 
         # Note: we store things in `self` so they don't get
         # garbage collected later on
-        c_verification = ffi.new("tanker_verification_t *", {"version": 5})
+        c_verification = ffi.new("tanker_verification_t *", {"version": 6})
         if isinstance(verification, VerificationKeyVerification):
             c_verification.verification_method_type = (
                 tankerlib.TANKER_VERIFICATION_METHOD_VERIFICATION_KEY
@@ -338,6 +356,15 @@ class CVerification:
             )
             self._passphrase = ffihelpers.str_to_c_string(verification.passphrase)
             c_verification.passphrase = self._passphrase
+
+        elif isinstance(verification, E2ePassphraseVerification):
+            c_verification.verification_method_type = (
+                tankerlib.TANKER_VERIFICATION_METHOD_E2E_PASSPHRASE
+            )
+            self._e2e_passphrase = ffihelpers.str_to_c_string(
+                verification.e2e_passphrase
+            )
+            c_verification.e2e_passphrase = self._e2e_passphrase
 
         elif isinstance(verification, EmailVerification):
             c_verification.verification_method_type = (
@@ -914,6 +941,7 @@ class Tanker:
         if options:
             c_verif_opts = CVerificationOptions(
                 with_session_token=options.with_session_token,
+                allow_e2e_method_switch=options.allow_e2e_method_switch,
             ).get()
         else:
             c_verif_opts = ffi.NULL
@@ -938,6 +966,7 @@ class Tanker:
         if options:
             c_verif_opts = CVerificationOptions(
                 with_session_token=options.with_session_token,
+                allow_e2e_method_switch=options.allow_e2e_method_switch,
             ).get()
         else:
             c_verif_opts = ffi.NULL
@@ -972,6 +1001,7 @@ class Tanker:
         if options:
             c_verif_opts = CVerificationOptions(
                 with_session_token=options.with_session_token,
+                allow_e2e_method_switch=options.allow_e2e_method_switch,
             ).get()
         else:
             c_verif_opts = ffi.NULL
